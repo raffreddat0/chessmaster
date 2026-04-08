b6080f6be0a954ae8cf3cd0499cbb072826257e2
Raffreddat0 <91798386+raffreddat0@users.noreply.github.com>
2026-04-08 16:43:45 +0200

    chessmaster

--- arduino/mega/pins.h
+++ arduino/mega/pins.h
@@ -2,12 +2,12 @@ int rows[] = {51, 47, 43, 39, 35, 31, 27, 23};
 int cols[] = {53, 49, 45, 41, 37, 33, 29, 25};
 
 //                RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
 int joyPin1 = A0;
 int joyPin2 = A1;
-int switchPin = 13;
+int switchPin = 6;
 
 int resetlcd = 53;
 
-int pinBreak = 48, pinBreakOutput = 46, pinMagnet = 6;
-int servoH = 5, servoM1 = 3, servoM2 = 2, servoUD = 4;
+int pinBreak = 48, pinBreakOutput = 46, pinMagnet = 44;
+int servoH = 5, servoM1 = 2, servoM2 = 3, servoUD = 4;
